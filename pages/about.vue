9ccb84f9fe3fa2fd5d27d1264521edf9f18d8c95
Will Gu <willgu29@gmail.com>
2017-08-22 20:53:08 -0700

    Filled in about and contact

--- pages/about.vue
+++ pages/about.vue
@@ -3,6 +3,17 @@
     <h1 class="title">
       About
     </h1>
+    <p id="about">
+      At their conception, EMRs were heralded as the way future healthcare
+      providers would improve workflows, catch near-misses, and analyze patient
+      and billing data. EMRs are already well under way in achieving many of these goals,
+      but their development is sporadic and uncoordinated across and even within healthcare
+      organizations. EMR Worx was created to allow individual providers to search, share,
+      and rank customizations that their colleagues have built for the benefit of themselves,
+      their respective organizations, and, most importantly, patient care. The EMR Worx team
+      invites you to share your templates,  macros, best practice advisories and more,
+      so that others may apply them.
+    </p>
 
   </section>
 </template>
@@ -22,5 +33,9 @@ export default {
 {
   margin: 15px 0;
 }
-
+#about {
+  margin-left: 100px;
+  margin-right: 100px;
+  text-align: left;
+}
 </style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -3,7 +3,8 @@
     <h1 class="title">
       Contact
     </h1>
-
+    <p><b><a href="mailto:info@emrworx.com">info@emrworx.com</a></b> for general inquiries.</p>
+    <p><b><a href="mailto:hello@emrworx.com">hello@emrworx.com</a></b> for everything else!</p>
   </section>
 </template>
 
@@ -18,8 +19,16 @@ export default {
 </script>
 
 <style scoped>
-.title
-{
+.title {
   margin: 15px 0;
 }
+b {
+  font-weight: 600;
+}
+a {
+  text-decoration: underline;
+  margin-right: 0px;
+  margin-left: 0px;
+}
+
 </style>
