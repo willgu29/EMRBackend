98a756b06334422061558ae42982571d89ffac7f
Will Gu <willgu29@gmail.com>
2017-09-06 13:09:57 -0700

    fixed zendesk

--- static/js/zendesk.js
+++ static/js/zendesk.js
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV !== 'production') return
-
-
 window.$zopim || (function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
 d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
 _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
