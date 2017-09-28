5992d280975d7ac773e92ac48e00971c3a1dafb2
Will Gu <willgu29@gmail.com>
2017-09-27 21:40:15 -0700

    updated landing page

--- pages/index.vue
+++ pages/index.vue
@@ -6,24 +6,79 @@
       Finish your notes faster
     </h2>
     <form action="/search" method="get">
-      <input placeholder="Search for note templates" class="search-bar" type="search" name="text" autocomplete="off" />
+      <input v-model='searchText' placeholder="Search for note templates" class="search-bar" type="search" name="text" autocomplete="off" />
     </form>
+
+    <div class='buttons'>
+      <input v-on:click="search" type='submit' class='button' value='Search' />
+      <input v-on:click="demo" type='submit' class='button' value='Demo' />
+    </div>
+
+    <br />
+    <br />
+    <br />
+    <hr>
+    <section>
+      <h1 class='title'>Note Templates by Department:</h1>
+      <div class="list-wrapper" v-if="users.length > 0">
+        <ul class="items">
+          <li v-for="(user, index) in users" :key="index" class="item">
+            <nuxt-link class='button' :to="{ name: 'users-id', params: { id: user._id }}">
+              {{ user.name }}: {{ user.institution }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </section>
 
   </section>
 </template>
 
 <script>
+import axios from '~/plugins/axios'
 
 export default {
+  asyncData ({ params, error }) {
+    console.log(params)
+    return axios.get('/api/users/')
+      .then((res) => {
+        return { users: res.data }
+      })
+      .catch((e) => {
+        error({ statusCode: 404, message: 'Users not found' })
+      })
+  },
   head () {
     return {
       title: 'EMR Worx'
+    }
+  },
+  data () {
+    return {
+      searchText: ''
+    }
+  },
+  methods: {
+    search: function () {
+      window.location.href = '/search?text=' + this.searchText
+    },
+    demo: function () {
+      window.location.href = '/demo'
     }
   }
 }
 </script>
 
 <style scoped>
+ul {
+  list-style: none;
+}
+.buttons {
+  margin-top: 20px;
+}
+.button {
+  margin: 5px 5px 5px 5px
+}
 .title
 {
   margin: 15px 0;
