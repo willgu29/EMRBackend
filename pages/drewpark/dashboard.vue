336d02c5baff9d31b1f290640de2756fe44ac4cf
Will Gu <willgu29@gmail.com>
2017-09-29 07:33:31 -0700

    Drew dashboard

--- pages/drewpark/dashboard.vue
+++ pages/drewpark/dashboard.vue
@@ -12,36 +12,72 @@
             <th>Last Bowel Movement</th>
             <th>% of Meals Eaten</th>
             <th>PRN Medications</th>
-            <th>Fishbone</th>
+            <th>Labs/Fishbone</th>
           </tr>
           <tr>
             <td>Silly Frog</td>
-            <td>#</td>
-            <td>##-##-##</td>
-            <td>###%</td>
-            <td>...</td>
-            <td><img src="https://storage.googleapis.com/emrworx.com/experimental/background.png"/></td>
+            <td>9</td>
+            <td>09-28-2017</td>
+            <td>100%</td>
+            <td>None</td>
+            <td>None</td>
           </tr>
           <tr>
             <td>Manson Worthfort</td>
-            <td>#</td>
-            <td>##-##-##</td>
-            <td>###%</td>
-            <td>...</td>
+            <td>6.25</td>
+            <td>09-28-2017</td>
+            <td>100%</td>
+            <td>None</td>
+            <td><img src="https://storage.googleapis.com/emrworx.com/experimental/slice1.png"/></td>
+
           </tr>
           <tr>
             <td>Slimy Popsicle</td>
             <td>#</td>
-            <td>##-##-##</td>
-            <td>###%</td>
-            <td>...</td>
+            <td>09-27-2017</td>
+            <td>100%</td>
+            <td>None</td>
+            <td>None</td>
           </tr>
           <tr>
             <td>Lila Lickington</td>
-            <td>#</td>
-            <td>##-##-##</td>
-            <td>###%</td>
-            <td>...</td>
+            <td>9</td>
+            <td>09-28-2017</td>
+            <td>100%</td>
+            <td>None</td>
+            <td>None</td>
+          </tr>
+          <tr>
+            <td>Final Lollipop</td>
+            <td>6</td>
+            <td>09-27-2017</td>
+            <td>100%</td>
+            <td>None</td>
+            <td><img src="https://storage.googleapis.com/emrworx.com/experimental/slice2.png"/></td>
+          </tr>
+          <tr>
+            <td>Hatty Monkey</td>
+            <td>7.25</td>
+            <td>09-28-2017</td>
+            <td>100%</td>
+            <td>alum hydrox/mag hydrox/simit 15 mL x 2, Atarax 25 mg x 1</td>
+            <td><img src="https://storage.googleapis.com/emrworx.com/experimental/slice3.png"/></td>
+          </tr>
+          <tr>
+            <td>Penguini Linguini</td>
+            <td>6.5</td>
+            <td>Denied Problems/No Complaints</td>
+            <td>100%</td>
+            <td>None</td>
+            <td><img src="https://storage.googleapis.com/emrworx.com/experimental/slice4.png"/></td>
+          </tr>
+          <tr>
+            <td>Lionel Pocketman</td>
+            <td>0.5</td>
+            <td>09-27-20</td>
+            <td>100%</td>
+            <td>Benadyrl 25 mg x 1, Atarax 10 mg x 1, Ambien 5 mg x 1</td>
+            <td>None</td>
           </tr>
         </thead>
       </table>
@@ -92,6 +128,7 @@ table {
   border: 2px solid #6093e5;;
   border-radius: 3px;
   background-color: #fff;
+  width: 80%;
 }
 
 th {
