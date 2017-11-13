2cc163f01425173336425f34eb3c9309d9717913
Will Gu <willgu29@gmail.com>
2017-11-12 22:36:16 -0800

    socket differetn server on prod

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,4 +1,11 @@
 import io from 'socket.io-client'
-const socket = io('http://localhost:3000')
+
+var socket
+
+if (process.env.NODE_ENV !== 'production') {
+  socket = io('https://www.emrworx.com')
+} else {
+  socket = io('http://localhost:3000')
+}
 
 export default socket
